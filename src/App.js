919ab6dc3c2598ff1255bec5304d65f704768467
gerardmi24 <gerardmistretta@yahoo.com>
2020-11-17 16:17:35 -0500

    Not finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+// import Objects from './src/Objects.js';
+import {yes, no} from './objects.js';
+
+//index > App > object.js
+//render 2 components (add an image tag)
+//make a container to put everything in
+
 function App() {
   return <h1>You Can Do This!</h1>;
 }
--- src/objects.js
+++ src/objects.js
@@ -3,3 +3,5 @@ const yes = { "yes-image": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AAN
 const no = { "no-image": "https://thumbs.gfycat.com/ElaborateShinyBernesemountaindog-size_restricted.gif", "no-statement": "learn problem solving, syntax, reading documentation" };
 
 export { yes, no };
+
+// export default Objects;
