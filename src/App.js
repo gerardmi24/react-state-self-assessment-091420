a2e64a75954ad4bc61b79e3068fea57a826d9208
gerardmi24 <gerardmistretta@yahoo.com>
2020-11-23 18:31:27 -0500

    Complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
+import { yes, no } from './objects.js';
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+// The DOM should first display the `no-image` and `no-statement` an `img` tag and `h1` tag
+
+// When the image is clicked, it should switch to the `yes-image` and `yes-statement`
+
+// When the image is clicked a second time it should flip back
+class App extends Component {
+
+state = {
+  clicked: false,
+}
+
+clickHandler = () => {
+  this.setState( {
+    clicked: !this.state.clicked
+  })
+}
+
+  render() {
+    return (
+      <div>
+        {this.state.clicked ? <h1> {yes["yes-statement"]}</h1>: <h1> {no["no-statement"]} </h1>}
+        <img onClick={() => this.clickHandler()} alt={"Drake"} src={this.state.clicked ? yes["yes-image"]: no["no-image"]} />
+      </div>
+    )
+  }
 }
 
 export default App;
