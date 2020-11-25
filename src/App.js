e08cf38d5cd2f3959c2eec007ca0d3a98b713c90
gerardmi24 <gerardmistretta@yahoo.com>
2020-11-25 18:01:32 -0500

    Chase gave me direction on how to refactor, will attempt tomorrow

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ clickHandler = () => {
 
   render() {
     let isClicked = this.state.clicked
+    console.log("State:", this.state, "Props:", this.props)
     return (
     <div>
-      
-      {isClicked? <div> <h1> {yes["yes-statement"]} </h1> <img onClick={this.clickHandler} src={yes["yes-image"]} /> </div>:
-       <div> <h1> {no["no-statement"]} </h1> <img onClick={this.clickHandler} src={no["no-image"]} /> </div>
+      {isClicked? <div> <h1> {yes["yes-statement"]} </h1> <img alt="Drake" onClick={this.clickHandler} src={yes["yes-image"]} /> </div>:
+       <div> <h1> {no["no-statement"]} </h1> <img alt="Drake" onClick={this.clickHandler} src={no["no-image"]} /> </div>
       }
     </div>
     )
@@ -28,3 +28,9 @@ clickHandler = () => {
 }
 
 export default App;
+
+//create two components (image and statement) - rcc
+//pass down as props in the image={the ternary but with yes or no image based on if clicked is true/false}
+//pass down as props in the statement={the ternary but with yes or no statement based on if clicked is true/false}
+//the image file will be passed the clickHandler
+
