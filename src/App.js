6e769f1e2607810cd6320dbec7f1e2a8b07f5452
gerardmi24 <gerardmistretta@yahoo.com>
2020-11-25 17:24:42 -0500

    Progress

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-function App() {
-  return <h1>You Can Do This!</h1>;
+import { yes, no } from './objects.js'
+
+class App extends Component {
+
+state = {
+  clicked: false
+}
+
+clickHandler = () => {
+  this.setState = {
+    clicked: !this.state.clicked
+  }
+}
+
+  render() {
+    let isClicked = this.state.clicked
+    return (
+    <div>
+      <h1> {no["no-statement"]} </h1>;
+      {isClicked?  <img onClick={this.clickHandler} src={yes["yes-image"]} />:
+       <img onClick={this.clickHandler} src={no["no-image"]} />
+      }
+    </div>
+    )
+  }
 }
 
 export default App;
